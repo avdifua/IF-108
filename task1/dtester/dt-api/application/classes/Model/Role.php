5f4b050e328d7e33591333d92048d29769f04d6e
Alex <avdifua@gmail.com>
2020-08-05 17:13:23 +0300

    moved folder dt-api

--- task1/dtester/dt-api/application/classes/Model/Role.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Model_Role extends Model_Auth_Role {
-	
-	protected $_table_columns = array("id" => "", "name" => "", "description" => "");
-	
-}
-
